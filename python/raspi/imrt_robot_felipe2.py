e9198bfb2333e3c0cd348c30c189e52b36780052
Felipe Radtke <feliperad@gmail.com>
2026-08-24 15:16:42 +0200

    tweaking

--- python/raspi/imrt_robot_felipe2.py
+++ python/raspi/imrt_robot_felipe2.py
@@ -86,7 +86,7 @@ while not motor_serial.shutdown_now :
     
 
     print(f'sending commands of {speed_motor_left} and {speed_motor_right}')
-    motor_serial.send_command(speed_motor_left, int(speed_motor_right))
+    #motor_serial.send_command(speed_motor_left, int(speed_motor_right))
 
     iteration_end_time = time.time()
     iteration_duration = iteration_end_time - iteration_start_time
