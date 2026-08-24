054b0a421fa187e92d42e6a1adbac6322bc0035d
Felipe Radtke <feliperad@gmail.com>
2026-08-24 13:24:29 +0200

    tweaking

--- python/raspi/imrt_robot_felipe2.py
+++ python/raspi/imrt_robot_felipe2.py
@@ -63,7 +63,7 @@ while not motor_serial.shutdown_now :
         previous_output = speed_motor_left
         speed_motor_right = 100
 
-    print(f'right motor value: {speed_motor_right} and correction is {correction}')
+    print(f'right motor value: {speed_motor_right}, error is {error}, kp*error is {kp*error}')
 
 
     motor_serial.send_command(speed_motor_left, speed_motor_right)
