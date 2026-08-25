41f15afbb07d196ad1661a5b2c74a6e5951416e6
Felipe Radtke <feliperad@gmail.com>
2026-08-25 13:45:36 +0200

    draft 3

--- python/raspi/uturn_test.py
+++ python/raspi/uturn_test.py
@@ -23,7 +23,7 @@ motor_serial.run()
 # print("Goodbye")
 
 t0 = time.time()
-while not motor_serial.shutdown_now and time.time() - t0 < 2:
+while not motor_serial.shutdown_now and time.time() - t0 < 4:
     motor_serial.send_command(200, 0)
     time.sleep(0.1)          # 10 Hz, mesma taxa do wall-following
 
