32fb42c0a60cd214643c2f4e353812e9e3178311
Felipe Radtke <feliperad@gmail.com>
2026-08-25 12:32:23 +0200

    draft 3

--- python/raspi/imrt_robot_felipe3_1.py
+++ python/raspi/imrt_robot_felipe3_1.py
@@ -7,7 +7,7 @@ from statistics import median
 # ---------------------------------------------------------------------------
 # Parametros de navegacao (os seus, sem alteracao)
 # ---------------------------------------------------------------------------
-min_threshold = 75
+min_threshold = 25
 median_threshold = 150
 max_threshold = 170
 front_threshold = 30
