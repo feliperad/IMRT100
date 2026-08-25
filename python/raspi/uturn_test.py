d4823773ce1395c7fd70589d4dbe336591fbcf9b
Felipe Radtke <feliperad@gmail.com>
2026-08-25 13:36:37 +0200

    draft 3

--- python/raspi/uturn_test.py
+++ python/raspi/uturn_test.py
@@ -12,9 +12,10 @@ except Exception:
 
 motor_serial.run()
 
-speed_motor_right = -200 ; speed_motor_left = 200
-motor_serial.send_command(speed_motor_left, speed_motor_right)
-time.sleep(3)
-motor_serial.send_command(0, 0)
+while not motor_serial.shutdown_now:
+    speed_motor_right = -200 ; speed_motor_left = 200
+    motor_serial.send_command(speed_motor_left, speed_motor_right)
+    time.sleep(3)
+    motor_serial.send_command(0, 0)
 
 print("Goodbye")
