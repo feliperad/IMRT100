b6f412b0374a6dc5fff161d23422c363f1f5b0eb
Felipe Radtke <feliperad@gmail.com>
2026-08-25 13:41:29 +0200

    draft 3

--- python/raspi/uturn_test.py
+++ python/raspi/uturn_test.py
@@ -12,12 +12,23 @@ except Exception:
 
 motor_serial.run()
 
-time.sleep(5)
+# time.sleep(5)
 
-while not motor_serial.shutdown_now:
-    speed_motor_right = -200 ; speed_motor_left = 200
-    motor_serial.send_command(speed_motor_left, speed_motor_right)
-    time.sleep(20)
+# while not motor_serial.shutdown_now:
+#     speed_motor_right = -200 ; speed_motor_left = 200
+#     motor_serial.send_command(speed_motor_left, speed_motor_right)
+#     time.sleep(20)
+#     motor_serial.send_command(0, 0)
+
+# print("Goodbye")
+
+t0 = time.time()
+while not motor_serial.shutdown_now and time.time() - t0 < 20:
+    motor_serial.send_command(200, -200)
+    time.sleep(0.1)          # 10 Hz, mesma taxa do wall-following
+
+for _ in range(5):
     motor_serial.send_command(0, 0)
+    time.sleep(0.05)
 
 print("Goodbye")
