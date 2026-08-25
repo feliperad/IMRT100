f6960cd94367f7bca17587a9a61eac6fc7965a36
Felipe Radtke <feliperad@gmail.com>
2026-08-25 15:38:50 +0200

    draft 3

--- python/raspi/imrt_robot_felipe3_1.py
+++ python/raspi/imrt_robot_felipe3_1.py
@@ -10,7 +10,7 @@ from statistics import median
 min_threshold = 10
 median_threshold = 20
 max_threshold = 200
-front_threshold = 20
+front_threshold = 5
 
 execution_frequency = 10
 execution_period = 1. / execution_frequency
@@ -144,7 +144,7 @@ while not motor_serial.shutdown_now:
     #     speed_motor_left = 0
 
     print(f'sending commands of {speed_motor_left} and {speed_motor_right}\n')
-    #motor_serial.send_command(speed_motor_left, speed_motor_right)
+    motor_serial.send_command(speed_motor_left, speed_motor_right)
 
     iteration_end_time = time.time()
     iteration_duration = iteration_end_time - iteration_start_time
