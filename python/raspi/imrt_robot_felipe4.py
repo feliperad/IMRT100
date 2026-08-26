7120192e471cf20393d46cd159886a875116d4c4
Felipe Radtke <feliperad@gmail.com>
2026-08-26 16:42:56 +0200

    draft 4

--- python/raspi/imrt_robot_felipe4.py
+++ python/raspi/imrt_robot_felipe4.py
@@ -141,7 +141,7 @@ while not motor_serial.shutdown_now:
         #     if turn_duration < execution_period:
         #         time.sleep(execution_period - turn_duration)
 
-        manobra(-100,100, 3.0)
+        manobra(-100,100, 1.5)
 
         motor_serial.send_command(0, 0)
 
