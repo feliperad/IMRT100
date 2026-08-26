4b135a4904df80d16833237abe3e74d087bb3670
Felipe Radtke <feliperad@gmail.com>
2026-08-26 16:48:35 +0200

    draft 4

--- python/raspi/imrt_robot_felipe4.py
+++ python/raspi/imrt_robot_felipe4.py
@@ -6,7 +6,7 @@ from statistics import median
 
 # ---------------------------------------------------------------- controle
 kp = 3.0
-ti = 600
+ti = 800
 td = 0.0                      # deixe em 0 ate o robo andar reto; ver notas
 
 setpoint = 20.0               # cm
