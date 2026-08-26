d35ba3ca04472269bcefedaca1530e6d1945f2ef
Felipe Radtke <feliperad@gmail.com>
2026-08-26 17:42:14 +0200

    draft 4

--- python/raspi/imrt_robot_felipe4.py
+++ python/raspi/imrt_robot_felipe4.py
@@ -227,7 +227,7 @@ while not motor_serial.shutdown_now:
         if dist_right > 60:
             print('manobras adicionais')
             manobra(base_speed, -base_speed, 3.5)
-            manobra(base_speed, base_speed, 3)
+            manobra(base_speed, base_speed, 2)
 
         print('going to state 0 - rastrear parede')
         rastrear_parede = True
